0b0e497161216af3bc34fdf967036ebe18e0be75
IlleniumDillon <147900130@qq.com>
2022-11-17 20:46:03 +0800

    add legacy code

--- main.c
+++ main.c
@@ -24,16 +24,46 @@
 
 int main()
 {
-    MM32DAC_pinInit(DAC1);
-    uint16_t i = 0;
-    while(1)
+  Menu_loadPara();
+
+  systick_start();
+  
+  MM32UART_TXPin txpin = {.port = GPIOD, .pin = P05, .conf = AF_PUSHPULL, .af = AF7, .moudle = UART2};
+  MM32UART_RXPin rxpin = {.port = GPIOD, .pin = P06, .conf = INPUT_FLOATING, .af = AF7, .moudle = UART2};
+  MM32UART_moudleInit(&txpin,&rxpin,NULL,NULL,1152000);
+  MM32_UART* m = (MM32_UART*)UART2;
+  m->IER.B.RX_IEN = 1;
+  nvic_init(UART2_IRQn, 0x02, 0x00, 1);
+  MM32UART_TXPin txpin2 = {.port = GPIOA, .pin = P00, .conf = AF_PUSHPULL, .af = AF8, .moudle = UART4};
+  MM32UART_RXPin rxpin2 = {.port = GPIOA, .pin = P01, .conf = INPUT_FLOATING, .af = AF8, .moudle = UART4};
+  MM32UART_moudleInit(&txpin2,&rxpin2,NULL,NULL,P18_device.baud);
+  
+  Menu_init();
+  Menu_display();
+
+  while(1)
+  {
+    static uint8_t flag = 0;
+    if(MM32GPIO_getPinState(GPIOA,P07))
     {
-        i++;
-        if(i == 4096)
-        {
-            i = 0;
-        }
-        MM32DAC_setData12(DAC1,i);
+      MM32PIT_Close(PIT1);
+      MM32PIT_Start(PIT2);
+      Menu_loop();
     }
-    return 0;
+    else
+    {
+      MM32PIT_Close(PIT2);
+      if(flag==0)
+      {
+        MM32PIT_timerTaskInit(PIT1,10,0);
+        flag = 1;
+      }
+      else
+      {
+        MM32PIT_Start(PIT1);
+      }
+    }
+    
+  }
+  return 0;
 }
